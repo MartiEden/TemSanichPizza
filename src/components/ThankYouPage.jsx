80190bb38ff5bc9009df1abce41a8544330a1b0b
artem ershov <arti.ershov@gmail.com>
2021-12-05 20:01:20 +0200

    thankyou page

--- src/components/ThankYouPage.jsx
+++ src/components/ThankYouPage.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import Image from "../assets/img/thanks-min.png"
 import { Link } from 'react-router-dom';
 
-const ThankYouPage = ({onBackToMain}) => {
+const ThankYouPage = ({ onBackToMain }) => {
 
     return (
         <div className="thankyou-page hidden">
-            <div className="thankyou-page__background"/>
-            <img className="thankyou-page__img" src={Image} alt="" />
+            <div className="thankyou-page__background" />
+            <div className="thankyou-page__content">
+                <img className="thankyou-page__content--img" src={Image} alt="" />
+                <Link to="/" onClick={onBackToMain} className="thankyou-page__content--button button button--black">
+                    <span>To the Main Page</span>
+                </Link>
+            </div>
 
-            <Link to="/" onClick={onBackToMain} className="thankyou-page__button button button--black">
-            <span>To the Main Page</span>
-          </Link>
+
+
         </div>
     )
 }
